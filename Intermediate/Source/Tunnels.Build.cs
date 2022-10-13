f1efb8088be58beafa4bd9fb75890f727199fb12
Émile Laforce <miloulune1@gmail.com>
2022-10-12 22:06:28 -0400

    End game

---
+++ Intermediate/Source/Tunnels.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Tunnels : ModuleRules
+{
+	public Tunnels(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
